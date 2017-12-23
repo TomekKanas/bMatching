d84ff509e58c37683a9ba44b01a88e15c1ef2f18
Tomasz Kanas <tk385674@students.mimuw.edu.pl>
2017-12-23 23:21:02 +0100

    Slightly optimized version - not working yet

--- adorators.cpp
+++ adorators.cpp
@@ -12,11 +12,14 @@
 #include <future>
 #include <algorithm>
 #include <cassert>
+#include <condition_variable>
 
 typedef std::pair<int, int> edge_t;
 
 const edge_t NULLEDGE = edge_t(-1, -1);
 const edge_t INFEDGE = edge_t(1e9 + 9, 1e9 + 9);
+const int NUM_COPY = 5;
+const int BREAK_MAIN = 0;
 
 std::map<int, int> in_map;
 std::vector<int> out_map;
@@ -34,7 +37,21 @@ std::vector<std::priority_queue<edge_t, std::vector<edge_t >, edge_compare> > S;
 std::vector<std::set<int> > T;
 std::vector<std::vector<edge_t > > v;
 std::vector<std::unique_ptr<std::mutex> > mut;
-std::mutex Smut, ochrona;
+std::mutex Smut, Qmut, Tmut, ochrona;
+std::condition_variable empty_queue, finish;
+int working = 0;
+
+
+
+void new_verticle(int num, int in_num) 
+{
+	in_map[in_num] = num;
+	out_map.push_back(in_num);
+	v.push_back(std::vector<edge_t >());
+	S.push_back(std::priority_queue<edge_t, std::vector<edge_t >, edge_compare>());
+	T.push_back(std::set<int>());
+	mut.push_back(std::make_unique<std::mutex>());
+}
 
 edge_t last(int b_method, int x) 
 {
@@ -54,77 +71,72 @@ void insert(int b_method, int u, edge_t edge)
 	//std::cout << "Insert: S(" << u << ") size = " << S[u].size()  << " b = " << bvalue(b_method, out_map[u]) << std::endl;
 }
 
-void new_verticle(int num, int in_num) 
-{
-	in_map[in_num] = num;
-	out_map.push_back(in_num);
-	v.push_back(std::vector<edge_t >());
-	S.push_back(std::priority_queue<edge_t, std::vector<edge_t >, edge_compare>());
-	T.push_back(std::set<int>());
-	mut.push_back(std::make_unique<std::mutex>());
-}
-
-edge_t find_edge(int b_method, int u)
+edge_t find_edge(int b_method, int u, std::set<int>& tempT)
 {
 	edge_compare cmp;
 	edge_t x = NULLEDGE; 
 	for(auto it = v[u].begin(); it != v[u].end(); ++it)
-		if(cmp(*it, x) && cmp(edge_t(u, it->second), last(b_method, it->first)) && T[u].count(it->first) == 0) x = *it;
+		if(cmp(*it, x) && cmp(edge_t(u, it->second), last(b_method, it->first)) && tempT.count(it->first) == 0) x = *it;
 	return x;
 }
 
-
-int suitor(const std::vector<int> Q, std::vector<int>& q, const std::vector<int>& b, std::vector<int>& db, int b_method)
+int suitor(std::vector<int>& Q, std::vector<int>& q, const std::vector<int>& b, std::vector<int>& db, int b_method)
 {
-	//std::cout << "Suitor function entered by thread: " << std::this_thread::get_id() << std::endl;
+	std::set<int> tempT{};
+	std::vector<std::pair<int, int>> inserted{};
+	std::vector<std::pair<int, int>> todelete{};
 	int i;
 	int res = 0; 
 	
-	
-	std::set<int> testing_set{1290, 1292};//, 2602, 2613, 6410, 6426};
-
-	
 	for(auto it = Q.begin(); it != Q.end(); ++it)
 	{
+		//TODO: wypróbować bez tego
+		Tmut.lock();
+		tempT = T[*it];
+		Tmut.unlock();
+
 		i = 0;
 		while(i < b[*it])
 		{
-			edge_t p = find_edge(b_method, *it);
+			edge_t p = find_edge(b_method, *it, tempT);
 			if(p != NULLEDGE)
 			{
-				std::lock_guard<std::mutex> lock(*mut[p.first]);
-				if(find_edge(b_method, *it) == p)
+				std::lock_guard<std::mutex> lock(*mut[p.first]); 
+				if(find_edge(b_method, *it, tempT) == p)
 				{
-					//if(testing_set.count(*it) > 0 || testing_set.count(p.first) > 0) std::cout << "Found edge: " << *it << " -> " << p.first << " value: " << p.second << std::endl;
 					++i;
 					edge_t y = last(b_method, p.first);
 					insert(b_method, p.first, std::make_pair(*it, p.second));
-					ochrona.lock();
-					T[*it].insert(p.first);
-					ochrona.unlock();
+					tempT.insert(p.first);
+					inserted.push_back(std::make_pair(*it, p.first));
 					res += p.second;
 
 					if(y != NULLEDGE)
 					{
-						/*if(testing_set.count(*it) > 0 || testing_set.count(p.first) || testing_set.count(y.first) > 0) 
-						{
-							std::cout << "Removing edge: " << y.first << " -> " << p.first << " value: " << y.second << std::endl;
-							std::cout << "Because of adding edge: " << *it << " -> " << p.first << " value: " << p.second << std::endl;
-						}*/
 						assert(y.second <= p.second);
-						std::lock_guard<std::mutex> lockk(ochrona);
-						T[y.first].erase(p.first);
-						q.push_back(y.first);
-						++db[y.first];
+						//T[y.first].erase(p.first);
+						todelete.push_back(std::make_pair(y.first, p.first));
+						//q.push_back(y.first);
+						//++db[y.first];
 						res -= y.second;
 					}
 				}
 			}
 			else break;
-			assert(T[*it].size() <= bvalue(b_method, out_map[*it]));
-			assert(S[*it].size() <= bvalue(b_method, out_map[*it]));
 		}
 	}
+	
+	ochrona.lock();
+	Tmut.lock();
+	for(auto it = inserted.begin(); it != inserted.end(); ++it) T[it->first].insert(it->second);
+	for(auto it = todelete.begin(); it != todelete.end(); ++it)
+	{
+		T[it->first].erase(it->second);
+		q.push_back(it->first);
+		++db[it->first];
+	}
+	Tmut.unlock();	
+	ochrona.unlock();
 	return res;
 }
 
@@ -141,7 +153,6 @@ int main(int argc, char* argv[])
    std::string input_filename{argv[2]};
 	std::ifstream infile;
 	infile.open(input_filename);
-	int res = 0;
 	int n_verticles = 0;
 	std::string line;
 	std::istringstream iss; 
@@ -166,45 +177,72 @@ int main(int argc, char* argv[])
 		}
 	}
 
-   for (int b_method = 0; b_method < b_limit + 1; b_method++) 
+	for (int b_method = 0; b_method < b_limit + 1; b_method++) 
 	{
+		std::vector<std::thread> threads;
 		std::vector<int> b,nb;
 		std::vector<int> Q,q;
-		std::vector<std::future<int> > futures;
+		int res = 0;
+		
 		for(int i = 0; i < n_verticles; ++i) 
 		{
 			b.push_back(bvalue(b_method, out_map[i]));
 			nb.push_back(0);
 			Q.push_back(i);
 		}
+
+		for(int i = 1; i < thread_count; ++i)
+			threads.push_back(std::thread([&res, &Q, &q, &b, &nb, b_method]
+			{
+				std::vector<int> toprocess{};
+				while(true)
+				{
+					std::unique_lock<std::mutex> lk(Qmut);
+					empty_queue.wait(lk, [Q]{return !Q.empty();});
+					for(int i = 0; i < NUM_COPY; ++i)
+					{
+						toprocess.push_back(Q.back());
+						Q.pop_back();
+						if(Q.empty()) break;
+					}
+					lk.unlock();
+					++working;
+					res += suitor(toprocess, q, b, nb, b_method);
+					--working;
+				}
+			}));
+
 		while(!Q.empty())
 		{
-			futures.clear();
-			int n = Q.size();
-			for(int i = 0; i < thread_count; ++i)
+			std::cout << Q.size() << std::endl;
+			std::vector<int> toprocess{};
+			//TODO: nie jestem pewien czy to przyspieszy
+			while(!Q.empty())
 			{
-				std::vector<int> pq{};
-				if(i == thread_count - 1) res += suitor(Q, q, b, nb, b_method);
-				else 
-					for(int j = 0; j < n / thread_count; ++j)
-					{
-						pq.push_back(Q.back());
-						Q.pop_back();
-					}
-				futures.push_back(std::async([pq, &q, &b, &nb, b_method]
-					{
-						return suitor(pq, q, b, nb, b_method);
-					}));
+				std::unique_lock<std::mutex> lk(Qmut);
+				if(Q.empty()) break;
+				for(int i = 0; i < NUM_COPY; ++i)
+				{
+					toprocess.push_back(Q.back());
+					Q.pop_back();
+					if(Q.empty()) break;
+				}
+				lk.unlock();
+				res += suitor(toprocess, q, b, nb, b_method);
 			}
-			for(auto it = futures.begin(); it != futures.end(); ++it) res += it->get();
+			std::unique_lock<std::mutex> lock(ochrona);
+			finish.wait(lock, []{return working == 0;});
+			//TODO: Nie jestem pewien czy to działa
+			for(int i = 0; i < n_verticles; ++i)
+			{
+				b[i] = nb[i];
+				nb[i] = 0;
+			}
 			std::sort(q.begin(), q.end());
-			Q.clear();
 			if(!q.empty()) Q.push_back(q.front());
 			for(size_t i = 1; i < q.size(); ++i)
 				if(q[i] != q[i - 1]) Q.push_back(q[i]);
 			q.clear();
-			b = nb;
-			for(int i = 0; i < n_verticles; ++i) nb[i] = 0;
 		}
 		std::cout << res/2 << std::endl;
 		res = 0;
@@ -214,4 +252,5 @@ int main(int argc, char* argv[])
 			T[i].clear();
 		}
    }
+	return 0;
 }
